5633457894d066e87bf5c00cd8a5095515aa9aeb
Adham Hamzawy <56726482+AdhamHamzawy@users.noreply.github.com>
2023-01-05 16:56:15 +0200

    edit in manifest

--- sale_auto_invoice/__manifest__.py
+++ sale_auto_invoice/__manifest__.py
@@ -3,11 +3,15 @@
     'name': "Sales Auto Invoice",
 
     'author': "Adham Hamzawy",
-
+     'description': """
+        after confirming sale order the rest of the process will be confirmed ,
+        ( sale order state will be confirmed ,  delivery order confirmed , invoice created ,
+        invoice payment done ) in one button click
+    """,
     # any module necessary for this one to work correctly 
-   # odoo16
+    
     'depends': ['base', 'sale', 'account'],
-
+    'version': '16',
     # always loaded
     'data': [
         'views/sale_view.xml',
